c319dfcac9c11ba0a5a506690d4e1afc48653701
Dan Purpura <danpurpura@GIR.local>
2012-08-12 13:27:52 -0400

    Added comments and rearranged the code
    The code that implements the interfaces is now at the bottom

--- CerealStore.php
+++ CerealStore.php
@@ -1,15 +1,139 @@
 <?php
 
+/**
+ * CerealStore
+ *
+ * A key-value store that serializes to a base64 encoded string.
+ *
+ * Example:
+ *
+ * $store = new CerealStore();
+ * $store->add('key', 'value)
+ *       ->add('key2', 'value2')
+ *       ->add('key3', 'value3');
+ *
+ * $serialized_string = (string)$store;
+ *
+ * $store2 = new CerealStore($serialized_string);
+ *
+ */
 class CerealStore implements Iterator, ArrayAccess, Countable {
 
 	protected $store = array();
 
+	/**
+	 * Constructor
+	 *
+	 * @param string - optional serialized data to unserialize into store
+	 */
 	public function __construct($data = null) {
 		if (isset($data)) {
 			$this->unserialize($data);
 		}
 	}
 
+	/**
+	 * add() - adds value to store
+	 *
+	 * @param string - key
+	 * @param mixed - value
+	 *
+	 * @return $this
+	 */
+	public function add($key, $value) {
+		return $this->offsetSet($key, $value);
+	}
+
+	/**
+	 * get() - returns the value for the given key
+	 *
+	 * @param string - key
+	 *
+	 * @return value - returns the value if the key is in the store, null otherwise
+	 */
+	public function get($key) {
+		return $this->offsetGet($key);
+	}
+
+	/**
+	 * has() - returns whether the key is in the store
+	 *
+	 * @param string - key
+	 *
+	 * @return bool - returns true if key exists in store, false otherwise
+	 */
+	public function has($key) {
+		return $this->offsetExists($key);
+	}
+
+	/**
+	 * remove() - removes the specified key from the store
+	 *
+	 * @param string - key
+	 *
+	 * @return $this
+	 */
+	public function remove($key) {
+		return $this->offsetUnset($key);
+	}
+
+	/**
+	 * addArray() - adds keys and values from array to the store
+	 *
+	 * @param array
+	 *
+	 * @return $this
+	 */
+	public function addArray($data) {
+		foreach($data as $key => $value) {
+			$this->add($key, $value);
+		}
+		return $this;
+	}
+
+	/**
+	 * serialize() - returns the contents of the store as base64 encoded string
+	 *
+	 * The store is JSON encoded, GZ deflated, and then base64 encoded.
+	 *
+	 * @return string
+	 */
+	public function serialize() {
+		return base64_encode(gzdeflate(json_encode($this->store)));
+	}
+
+	/**
+	 * unserialize() - sets the store to the contents of the serialized data
+	 *
+	 * @param mixed - a serialized string or object (cast to a string)
+	 *
+	 * If the data cannot be unserialized, the store will be an empty array
+	 *
+	 * @return $this
+	 */
+	public function unserialize($data) {
+		// if received an object, replace it with the string
+		// this allows us to do $store->unserialize($store2)
+		if (is_object($data)) {
+			$data = $data->__toString();
+		}
+
+		// try to unserialize the data, if anything fails set the store to an empty
+		// array
+		if ($store = json_decode(gzinflate(base64_decode($data)))) {
+			$this->store = (array)$store;
+		} else {
+			$this->store = array();
+		}
+	
+		return $this; 
+	}
+	
+	public function __toString() {
+		return $this->serialize($this);
+	}
+
+	// Iterator
 	public function current() {
 		return current($this->store);
 	}
@@ -30,10 +154,12 @@ class CerealStore implements Iterator, ArrayAccess, Countable {
 		return key($this->store) !== null;
 	}
 
+	// Countable
 	public function count() {
 		return count($this->store);
 	}
 
+	// Array Access
     public function offsetSet($key, $value) {
         if (is_null($key)) {
             $this->store[] = $value;
@@ -45,6 +171,9 @@ class CerealStore implements Iterator, ArrayAccess, Countable {
     }
 
     public function offsetExists($key) {
+	    // isset works in all cases except when the value is null,
+	    // so fallback to array_key_exists in that case
+	    // this offers a slight performance gain over just using array_key_exists
         return isset($this->store[$key]) || array_key_exists($key, $this->store);
     }
 
@@ -56,49 +185,4 @@ class CerealStore implements Iterator, ArrayAccess, Countable {
     public function offsetGet($key) {
         return (isset($this->store[$key]) ? $this->store[$key] : null);
     }
-
-	public function serialize() {
-		return base64_encode(gzdeflate(json_encode($this->store)));
-	}
-
-	public function unserialize($data) {
-		if (is_object($data)) {
-			$data = $data->__toString();
-		}
-
-		if ($store = json_decode(gzinflate(base64_decode($data)))) {
-			$this->store = (array)$store;
-		} else {
-			$this->store = array();
-		}
-
-		return $this; 
-	}
-
-	public function add($key, $value) {
-		return $this->offsetSet($key, $value);
-	}
-
-	public function get($key) {
-		return $this->offsetGet($key);
-	}
-
-	public function has($key) {
-		return $this->offsetExists($key);
-	}
-
-	public function remove($key) {
-		return $this->offsetUnset($key);
-	}
-
-	public function addArray($data) {
-		foreach($data as $key => $value) {
-			$this->add($key, $value);
-		}
-		return $this;
-	}
-
-	public function __toString() {
-		return $this->serialize($this);
-	}
 }
